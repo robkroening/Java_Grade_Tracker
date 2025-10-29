483439af911b823f45cb2e19f46498b2bb757fb4
robkroening <rob.kroening@gmail.com>
2025-10-29 14:45:57 -0400

    added setup classes and info and some in Course class

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // adds a new student
+        // add a course
+        // give student a grade for course
+        // view students report card
+        // see their gpa
+        // storing --->
+        // many students
+        // many courses
+        // which student took which course and what they scored
+
+        // classes -------->
+        // Student.java
+        // Course.java
+        // Grade.java
+        // School.java
+
+        // remember each method should only do ONE thing like printing OR returning a
+        // value
+    }
+}
